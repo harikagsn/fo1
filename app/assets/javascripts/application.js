b1a4171eeaee1fa45bd68f56866779ad50b36ff1
harikagsn <harikagoruganthu13@gmail.com>
2018-06-18 10:42:11 +0530

    Changed Gemfile for Heroku

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.sidenav');
+   var instances = M.Sidenav.init(elems, options);
+ });
+
+ // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+ // var collapsibleElem = document.querySelector('.collapsible');
+ // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('.sidenav').sidenav();
+ });
